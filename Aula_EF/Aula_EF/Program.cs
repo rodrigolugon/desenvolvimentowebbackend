fcd9c01e10e640e512ee2bcd45a68ab2d0314575
Rodrigo Lugon <rodrigolugonn@gmail.com>
2026-03-16 22:33:01 -0300

    Aplicando gitignore

--- Aula_EF/Aula_EF/Program.cs
+++ Aula_EF/Aula_EF/Program.cs
@@ -1,25 +1,45 @@
+using Aula_EF.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseDefaultServiceProvider(options => {
+   // options.ValidateScopes = false; // Ativa a validacao de escopo. False evita erro de BD inexistente
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//ConfiguraCAOo da Entity Framework Core
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlServer(builder.Configuration["Data:Exemplo_EF_BD26:ConnectionString"],
+
+    //evita que o BD n?o seja criado por problemas de timeout com o servidor
+    sqlServerOptionsAction: sqlOptions => {
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 10,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null);
+    }));
+
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Home/Error");
 }
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
+//SeedData.Initialize(app.Services);
+SeedData.EnsurePopulated(app);
 
 app.Run();
